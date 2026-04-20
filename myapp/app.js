ad012f22773f2c959c8256267dfd9f3b521c2cf4
poorna138 <31821528+poorna138@users.noreply.github.com>
2026-04-20 17:39:44 +0530

    Update app.js

--- myapp/app.js
+++ myapp/app.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  console.log(`Request received: ${req.url}`);
   res.end('Hello from CI/CD pipeline 🚀');
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
+
+
